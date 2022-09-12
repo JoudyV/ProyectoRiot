70d93d44afa5a8272a150edee56846b5c9bfb272
JoudyV <102838211+JoudyV@users.noreply.github.com>
2022-09-12 13:03:31 -0300

    valorant inicio

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+
+  user: string;
+  pass: string;
+  islogged: boolean;
+  
 
   constructor() { }
 
